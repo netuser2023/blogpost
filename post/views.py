8cc559741a84d658fa9c0823a3308834d777949a
netuser2023 <wittechuz@gmail.com>
2023-11-14 17:54:57 +0500

    Change commit

--- post/views.py
+++ post/views.py
@@ -5,12 +5,10 @@ from django.http import HttpResponse
 def posts(request):
     content = {
         'content':[
-            {'Title': 'America', 'Id': '2390', 'Createdate':'12/03/2023'},
-            {'Title': 'Chicago', 'Id': '2930', 'Createdate':'26/09/2023'},
-            {'Title': 'Moscow', 'Id': '3245', 'Createdate':'03/10/2023'},
-            {'Title': 'Tashkent', 'Id': '4859', 'Createdate':'29/05/2023'},
-            {'Title': 'Mercedes', 'Id': '6754', 'Createdate':'14/07/2023'},
-            {'Title': 'Audi RS7', 'Id': '9978', 'Createdate':'19/02/2023'},
+            {'Title': 'America', 'Id': '2390', 'Createdate':'12/03/2023', 'image':'images/america.jpg'},
+            {'Title': 'Chicago', 'Id': '2930', 'Createdate':'26/09/2023', 'image':'images/chicago.jpg'},
+            {'Title': 'Moscow', 'Id': '3245', 'Createdate':'03/10/2023', 'image':'images/moscow.jpg'},
+            {'Title': 'Dubai', 'Id': '4859', 'Createdate':'29/05/2023', 'image':'images/dubai.jpg'},
             ]
     }
     return render(request=request, template_name='main.html', context=content)
